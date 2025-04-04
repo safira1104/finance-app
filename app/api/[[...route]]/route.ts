b965c922449890779a22406583c6f6837fe4a7d2
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-04-04 14:34:25 +0700

    feat(accounts): add accounts API with authentication
    - Added GET /accounts to fetch user accounts
    - Added POST /accounts to create new accounts with validation
    - Integrated Clerk for authentication
    - Used Drizzle ORM for database operations

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,17 +1,15 @@
-import { z } from 'zod';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel';
-import { zValidator } from '@hono/zod-validator';
 
-
+import accounts from './accounts';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get("/hello", (c) => {
-    return c.json({hello: "World" });
-});
+const routes = app.route("/accounts", accounts);
   
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,11 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
-
+import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
     id: text("id").primaryKey(),
     name: text("name").notNull(),
     userId: text("user_id").notNull(),
 });
+
+export const insertAccountSchema = createInsertSchema(accounts);
 
