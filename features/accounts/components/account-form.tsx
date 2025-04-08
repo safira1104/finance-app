f5f481793b43318258669fab085aee24daaea740
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-04-08 09:28:15 +0700

    Add AccountForm component with validation using Zod and React Hook Form

--- features/accounts/components/account-form.tsx
+++ features/accounts/components/account-form.tsx
@@ -1,6 +1,6 @@
-import { z } from "zod";
+import { Values, z } from "zod";
 import { Trash } from "lucide-react";
-import { useForm } from "react-hook-form";
+import { useForm, UseFormSetValue } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Input } from "@/components/ui/input";
@@ -8,3 +8,68 @@ import { Button } from "@/components/ui/button";
 import { insertAccountSchema } from "@/db/schema";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
+const formSchema = insertAccountSchema.pick({
+    name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+    id?: string;
+    defaultValues?: FormValues;
+    onSubmit: (Values: FormValues) => void;
+    onDelete?: () => void;
+    disabled?: boolean;
+};
+
+export const AccountForm  = ({
+    id, 
+    defaultValues,
+    onSubmit,
+    onDelete,
+    disabled,
+}: Props) => {
+    const form = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: defaultValues,
+    });
+
+    const handleSubmit = (values: FormValues) => {
+        console.log({ values });
+    }
+
+    const handleDelete = () => {
+        onDelete?.();
+    };
+
+    return (
+        <Form {...form}>
+            <form 
+                onSubmit={form.handleSubmit(handleSubmit)}
+                className="space-y-4 pt-4"
+            >
+                <FormField 
+                    name="name"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Name
+                            </FormLabel>
+                            <FormControl>
+                                <Input
+                                    disabled={disabled}
+                                    placeholder="e.g. Cash, Bank, Credit Card"
+                                    {...field}
+                                />
+                                
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+
+            </form>
+
+        </Form>
+    )
+};
