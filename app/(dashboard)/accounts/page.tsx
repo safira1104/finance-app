9585773adcde95904caf4a7235a6333e84b7fcf7
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-04-11 17:14:16 +0700

    feat: implement Accounts Page with sortable table, add UI components (Card, Table), and update dependencies

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+
+import { columns, Payment } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+  const data: Payment[] =  [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+        id: "728ed543",
+        amount: 500,
+        status: "success",
+        email: "a@example.com",
+      },
+  ]
+
+const AccountsPage = () => {
+
+    const newAccount = useNewAccount();
+
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-center">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Account Page
+                    </CardTitle>
+                    <Button onClick={newAccount.onOpen} className="sm">
+                        <Plus className="size-4 mr-2"/>
+                        Add new
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                    <DataTable columns={columns} data={data} />
+                </CardContent>
+            </Card>
+        </div>
+    );
+};
+
+export default AccountsPage;
